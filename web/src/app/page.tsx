5ab1f33df036537eed275c659c69cc2e713c8828
matheusmagnani <mrmatheusmagnani@gmail.com>
2025-02-19 11:56:26 -0300

    feat: now we have the input component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,12 @@
-import { ArrowRight, Copy } from 'lucide-react'
+import { ArrowRight, Copy, Mail } from 'lucide-react'
 import { Button } from "@/components/button";
 import { IconButton } from '@/components/icon-button';
+import { InputField, InputIcon, InputRoot } from '@/components/input';
 
 export default function Home() {
   return (
     <main>
-      <Button>
+      <Button type= "submit">
         Label
         <ArrowRight/>
       </Button>
@@ -13,6 +14,17 @@ export default function Home() {
       <IconButton>
          <Copy/>
       </IconButton>
+
+      <div>
+        <InputRoot error>
+
+          <InputIcon>
+            <Mail className='size-5'/>
+          </InputIcon>
+          <InputField  placeholder="Digite seu email" />
+
+        </InputRoot>
+      </div>
 
     </main>
   );
--- web/src/components/button.tsx
+++ web/src/components/button.tsx
@@ -1,14 +1,11 @@
-import { ReactNode } from "react";
+import { ComponentProps } from "react";
 
-interface ButtonProps {
-  children?: ReactNode;
-}
+interface ButtonProps extends ComponentProps<'button'> {}
 export function Button(props: ButtonProps) {
   return (
     <button 
       className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900 "
-    >
-     { props.children }
-    </button>
+      {...props}
+    />
   )
 }
